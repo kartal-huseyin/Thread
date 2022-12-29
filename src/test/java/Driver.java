34960b33eff2deb516edf7a0c0079b553fa0ae57
hkartal <huseyin86kartal@gmail.com>
2022-12-29 18:16:29 +0300

    plugins

--- src/test/java/Driver.java
+++ src/test/java/Driver.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterTest;
 
 public class Driver {
 
@@ -42,5 +43,10 @@ public class Driver {
             drivers.get().quit();
     }
 
+
+    @AfterTest
+    public void afterTest(){
+        Driver.quitDriver();
+    }
 
 }
