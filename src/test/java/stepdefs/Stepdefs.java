df572f98d1c1d1b5f9f358c4c4371d26726fec0f
hkartal <huseyin86kartal@gmail.com>
2022-12-29 20:14:49 +0300

    plugins

--- src/test/java/stepdefs/Stepdefs.java
+++ src/test/java/stepdefs/Stepdefs.java
@@ -1,4 +1,23 @@
 package stepdefs;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class Stepdefs {
+
+
+    @Given("one")
+    public void one() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("two")
+    public void two() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
 }
